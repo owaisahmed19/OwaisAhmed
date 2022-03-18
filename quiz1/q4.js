b9433e1d59b5a6fae69870a5f598a2fd733246f3
owaisahmed19 <101355018+owaisahmed19@users.noreply.github.com>
2022-03-18 14:15:24 +0500

    Create q4.js

---
+++ quiz1/q4.js
@@ -0,0 +1,4 @@
+let myArray=['a','b','c','d'];
+myArray.unshift('start');  //added element to the start of the array
+myArray.push('end');        //added element to the end of the array
+console.log(myArray);
