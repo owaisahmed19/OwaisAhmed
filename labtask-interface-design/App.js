4f7b819e46420a8bb0886a1957549148e3d75264
owaisahmed19 <101355018+owaisahmed19@users.noreply.github.com>
2022-03-26 12:57:33 +0500

    Update App.js

--- labtask-interface-design/App.js
+++ labtask-interface-design/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import ButtonStyle from "./Components/ButtonStyle";
 import Comm from "./Components/comm";
-import Interface from "./Components/Pic";
+import Interface from "./Components/Interface";
 
 const flex = () => {
   return <Interface></Interface>;
