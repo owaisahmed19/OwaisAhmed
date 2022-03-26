ef2ff32b5d6ca4121a896ad1d2c3aecd519d108a
owaisahmed19 <101355018+owaisahmed19@users.noreply.github.com>
2022-03-26 12:52:00 +0500

    Create App.js

---
+++ labtask-interface-design/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ButtonStyle from "./Components/ButtonStyle";
+import Comm from "./Components/comm";
+import Pic from "./Components/Pic";
+
+const flex = () => {
+  return <Pic></Pic>;
+};
+export default flex;
