4ae479ef2dc69c9c5eb34e294e0f6029fdab62a1
owaisahmed19 <101355018+owaisahmed19@users.noreply.github.com>
2022-04-01 23:03:37 +0500

    Create App.js

---
+++ labassignment 02/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ButtonStyle from "./Components/ButtonStyle";
+import Comm from "./Components/comm";
+import Interface from "./Components/Interface";
+import Login from "./Components/Login";
+
+const flex = () => {
+  return <Login></Login>;
+};
+export default flex;
