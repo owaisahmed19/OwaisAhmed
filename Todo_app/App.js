519e85e5c9b61ce86652fd1a0326662579b6d3fb
owaisahmed19 <101355018+owaisahmed19@users.noreply.github.com>
2022-04-11 00:07:29 +0500

    Create App.js

---
+++ Todo_app/App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image
+} from 'react-native';
+import ButtonStyle from './Components/ButtonStyle';
+import Todo from './Todo/Todo'
+
+
+
+const flex = () =>{
+  return(
+  
+    <Todo></Todo>
+  )
+}
+
+
+export default flex;
